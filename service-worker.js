393f7f58be46062b4a951b3552bb6ed32751acac
Nadiyanisa <auliyanadiya29@gmail.com>
2024-11-08 21:52:46 +0700

    secondcommit

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,22 @@
 const cacheName = "portfolio-cache-v1";
 const assetsToCache = [
   "/",
+  "/manifest.json",
+  "/preview.png",
+  "/service-worker.js",
   "/index.html",
   "/css/style.css",
-  "/js/script.js",
-  "/assets/img/"
-  "/assets/img/about.jpg"
-  "/assets/img/perfil.png"
-  "/assets/img/work1.jpg"
-  "/assets/img/work2.jpg"
-  "/assets/img/work3.jpg"
-  "/assets/img/work4.jpg"
-  "/assets/img/work5.jpg"
-  "/assets/img/work6.jpg"
+  "/assets/scss/styles.scss",
+  "/js/main.js",
+  "/assets/img/",
+  "/assets/img/about.jpg",
+  "/assets/img/perfil.png",
+  "/assets/img/work1.jpg",
+  "/assets/img/work2.jpg",
+  "/assets/img/work3.jpg",
+  "/assets/img/work4.jpg",
+  "/assets/img/work5.jpg",
+  "/assets/img/work6.jpg",
   // tambahkan semua file yang ingin Anda cache
 ];
 
